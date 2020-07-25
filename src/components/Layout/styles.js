11dd0e86c784267d744f9a4319a294a36864b82f
Jose Cuevas <josecueram@gmail.com>
2020-07-25 15:53:20 -0500

    [UPDATE] Layout Styles

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Arranger = styled.div`
   display: grid;
-  grid-template-columns: 150px 1fr;
+  grid-template-columns: 100px 1fr;
   grid-template-rows: 100vh;
   gap: 15px;
 `
