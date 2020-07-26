4caf9a50d46d92284c6b5aa10faa7a45b95487a1
Jose Cuevas <josecueram@gmail.com>
2020-07-26 18:57:40 -0500

    [UPDATE] Added placeholder for BigPicture Component

--- src/components/BigCard/index.jsx
+++ src/components/BigCard/index.jsx
@@ -1,22 +1,25 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { Context } from '../../Context'
-import { Loader } from '../Loader'
 
-import { Card, Figure, ThumbImg, LoadedImage, H2 } from './styles'
+import { Card, ThumbImg, LoadedImage, H2 } from './styles'
 
 export const BigCard = () => {
   const { bigPicture, thumbPicture, bigPictureLoaded, setBigPictureLoaded } = useContext(Context)
 
-  console.log('BigCard -> bigPictureLoading', bigPictureLoaded)
-
   const handleOnLoad = () => {
-    console.log('finished loading')
     setBigPictureLoaded(true)
   }
+
   return (
     <Card>
-      <ThumbImg src={thumbPicture} isLoaded={bigPictureLoaded} />
-      <LoadedImage onLoad={handleOnLoad} src={bigPicture} isLoaded={bigPictureLoaded} />
+      {
+        bigPicture ? (
+          <>
+            <ThumbImg src={thumbPicture} isLoaded={bigPictureLoaded} />
+            <LoadedImage onLoad={handleOnLoad} src={bigPicture} isLoaded={bigPictureLoaded} />
+          </>
+        ) : <H2>Select an image</H2>
+      }
     </Card>
   )
 }
