bf98b2ac1c97cafc2edd0b6efd70cca682bfa0ef
Jose Cuevas <josecueram@gmail.com>
2020-07-26 19:02:12 -0500

    [FIX] BigCard False Loading

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,12 +4,14 @@ import { Context } from '../../Context'
 import { Div, Img, Figure } from './styles'
 
 export const Card = ({ id, imageUrl, imageUrlHiRes }) => {
-  const { setBigPicture, setThumbPicture, setBigPictureLoaded } = useContext(Context)
+  const { setBigPicture, setThumbPicture, bigPicture, setBigPictureLoaded } = useContext(Context)
 
   const handleOnClick = () => {
-    setThumbPicture(imageUrl)
-    setBigPictureLoaded(false)
-    setBigPicture(imageUrlHiRes)
+    if (bigPicture !== imageUrlHiRes) {
+      setThumbPicture(imageUrl)
+      setBigPictureLoaded(false)
+      setBigPicture(imageUrlHiRes)
+    }
   }
 
   return (
