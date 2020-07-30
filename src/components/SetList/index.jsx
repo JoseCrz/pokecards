9d840d65be4d189f1e0a5742a2db93739748936d
Jose Cuevas <josecueram@gmail.com>
2020-07-29 23:25:21 -0500

    [ADD] Created Bare SetList Component

---
+++ src/components/SetList/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Set } from '../Set'
+
+import { Container } from './styles'
+export const SetList = ({ sets = [] }) => {
+  return (
+    <Container>
+      {
+        sets.map(set => <Set key={set.code} {...set} />)
+      }
+    </Container>
+  )
+}
---
+++ src/components/SetList/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  grid-auto-rows: 150px;
+  gap: 15px;
+`
