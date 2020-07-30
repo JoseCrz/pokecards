ddef46f25c5df53145acd6e5b96f3fd279a6250e
Jose Cuevas <josecueram@gmail.com>
2020-07-29 23:26:50 -0500

    [ADD] Implemented SetList Component in Set Page

--- src/pages/Sets/index.jsx
+++ src/pages/Sets/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 import { Layout } from '../../components/Layout'
+import { SetList } from '../../components/SetList'
+
+import { setsMock } from '../../mocks/dataMock'
 
 export const Sets = () => {
   return (
-    <Layout title='Sets' />
+    <Layout title='Sets'>
+      <SetList sets={setsMock} />
+    </Layout>
   )
 }
