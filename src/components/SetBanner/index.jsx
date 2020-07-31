86eca431f5b02a766122699ec0d7aafb48c528ce
Jose Cuevas <josecueram@gmail.com>
2020-07-30 21:45:37 -0500

    [ADD] Created Basic SetBanner Component Styles

--- src/components/SetBanner/index.jsx
+++ src/components/SetBanner/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import { Banner } from './styles'
+import { Banner, TextContainer, Img, P } from './styles'
 
 export const SetBanner = ({ logoUrl, series, totalCards, releaseDate }) => {
   return (
     <Banner>
-      <img src={logoUrl} alt='' />
-      <p>{`Series: ${series}`}</p>
-      <p>{`Total Cards: ${totalCards}`}</p>
-      <p>{`Release Date: ${releaseDate}`}</p>
+      <Img src={logoUrl} alt='' />
+      <TextContainer>
+        <P>{`Series: ${series}`}</P>
+        <P>{`Total Cards: ${totalCards}`}</P>
+        <P>{`Release Date: ${releaseDate}`}</P>
+      </TextContainer>
     </Banner>
   )
 }
--- src/components/SetBanner/styles.js
+++ src/components/SetBanner/styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components'
 
 export const Banner = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
+export const TextContainer = styled.div`
+  display: flex;
+`
+
+export const P = styled.p`
+  margin-right: 10px;
+  color: darkgray;
+`
+
+export const Img = styled.img`
+  max-height: 60%;
 `
