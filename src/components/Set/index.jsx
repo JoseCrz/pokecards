6f42bf09f3687065b7f572ef359a929e01eae972
Jose Cuevas <josecueram@gmail.com>
2020-07-31 00:55:51 -0500

    [REMOVE] Redundant logic in Set Component

--- src/components/Set/index.jsx
+++ src/components/Set/index.jsx
@@ -1,20 +1,14 @@
-import React, { useContext } from 'react'
-import { Context } from '../../Context'
+import React from 'react'
 import { Link } from '@reach/router'
 
 import { Container, Img } from './styles'
 
 export const Set = ({ set }) => {
-  const { setCurrentSet } = useContext(Context)
   const { code, logoUrl } = set
-
-  const handleOnClick = () => {
-    setCurrentSet(set)
-  }
 
   return (
     <Link to={`/sets/${code}`}>
-      <Container onClick={handleOnClick}>
+      <Container>
         <Img src={logoUrl} alt='' />
       </Container>
     </Link>
