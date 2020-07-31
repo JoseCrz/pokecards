d37d93c2c29409e2512e248a23e50e6156fe9ac2
Jose Cuevas <josecueram@gmail.com>
2020-07-30 20:52:49 -0500

    [ADD] Created Basic SetDetail Grid Layout

--- src/pages/SetDetail/index.jsx
+++ src/pages/SetDetail/index.jsx
@@ -3,8 +3,10 @@ import { Context } from '../../Context'
 
 import { Layout } from '../../components/Layout'
 import { SetBanner } from '../../components/SetBanner'
+import { CardList } from '../../components/CardList'
+import { BigCard } from '../../components/BigCard'
 
-import { } from './style'
+import { Grid, BannerContainer, CardListContainer, BigCardContainer } from './style'
 
 export const SetDetail = () => {
   const { currentSet } = useContext(Context)
@@ -12,7 +14,17 @@ export const SetDetail = () => {
 
   return (
     <Layout title={name || 'Set name'}>
-      <SetBanner {...currentSet} />
+      <Grid>
+        <BannerContainer>
+          <SetBanner {...currentSet} />
+        </BannerContainer>
+        <CardListContainer>
+          <CardList />
+        </CardListContainer>
+        <BigCardContainer>
+          <BigCard />
+        </BigCardContainer>
+      </Grid>
     </Layout>
   )
 }
--- src/pages/SetDetail/style.js
+++ src/pages/SetDetail/style.js
@@ -1,1 +1,21 @@
 import styled from 'styled-components'
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 150px 1fr;
+  grid-template-areas: 'banner banner'
+                       'cardList bigCard';
+`
+
+export const BannerContainer = styled.div`
+  grid-area: banner;
+`
+
+export const CardListContainer = styled.div`
+  grid-area: cardList;
+`
+
+export const BigCardContainer = styled.div`
+  grid-area: bigCard;
+`
