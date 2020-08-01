02b3a062cbd3f028d4f8eba3fca49d2120aff9d1
Jose Cuevas <josecueram@gmail.com>
2020-08-01 01:58:40 -0500

    [UPDATE] Improved Navbar Icons UI/UX

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,34 +1,56 @@
 import React, { useContext } from 'react'
 import { Link } from '@reach/router'
+import { IconContext } from 'react-icons'
+import {
+  AiFillHome as IconHomeFull,
+  AiOutlineHome as IconHome,
+  AiOutlineSetting as IconSettings,
+  AiFillSetting as IconSettingsFull,
+  AiOutlineStar as IconFavs,
+  AiFillStar as IconFavsFull
+} from 'react-icons/ai'
+
+import { BsCollection as IconSets, BsFillCollectionFill as IconSetsFull } from 'react-icons/bs'
 import { Context } from '../../Context'
 
-import { Nav, Ul, IconHome, IconSettings, IconFav, SetsIncon } from './styles'
+import { Nav, Ul } from './styles'
 export const Navbar = () => {
   const { setSearchString } = useContext(Context)
+
   return (
-    <Nav>
-      <Ul>
-        <li>
-          <Link to='/'>
-            <IconSettings />
-          </Link>
-        </li>
-        <li>
-          <Link to='/sets'>
-            <SetsIncon />
-          </Link>
-        </li>
-        <li>
-          <Link to='/'>
-            <IconHome onClick={() => setSearchString('')} />
-          </Link>
-        </li>
-        <li>
-          <Link to='/'>
-            <IconFav />
-          </Link>
-        </li>
-      </Ul>
-    </Nav>
+    <IconContext.Provider value={{ color: 'white', size: '36px' }}>
+      <Nav>
+        <Ul>
+          <li>
+            <Link to='/settings'>
+              {
+                window.location.pathname.includes('/settings') ? <IconSettingsFull /> : <IconSettings />
+              }
+            </Link>
+          </li>
+          <li>
+            <Link to='/sets'>
+              {
+                window.location.pathname.includes('/sets') ? <IconSetsFull /> : <IconSets />
+              }
+            </Link>
+          </li>
+          <li>
+            <Link to='/' onClick={() => setSearchString('')}>
+              {
+                window.location.pathname === '/' ? <IconHomeFull /> : <IconHome />
+              }
+            </Link>
+          </li>
+          <li>
+            <Link to='/favs'>
+              {
+                window.location.pathname.includes('/favs') ? <IconFavsFull /> : <IconFavs />
+              }
+            </Link>
+          </li>
+        </Ul>
+      </Nav>
+    </IconContext.Provider>
   )
 }
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,12 +1,4 @@
 import styled from 'styled-components'
-import { AiFillHome, AiFillStar } from 'react-icons/ai'
-import { IoMdOptions } from 'react-icons/io'
-import { BsCardList } from 'react-icons/bs'
-
-const generalStyle = `
-  color: var(--text-white);
-  font-size: 36px;
-`
 
 export const Nav = styled.nav`
   background: var(--primary);
@@ -20,19 +12,4 @@ export const Ul = styled.ul`
   justify-content: space-between;
   align-items: center;
   padding: 20px 0;
-`
-
-export const IconHome = styled(AiFillHome)`
-  ${generalStyle}
-`
-
-export const IconSettings = styled(IoMdOptions)`
-  ${generalStyle}
-`
-export const IconFav = styled(AiFillStar)`
-  ${generalStyle}
-`
-
-export const SetsIncon = styled(BsCardList)`
-  ${generalStyle}
 `
