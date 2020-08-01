dd252d0498d5abb9afa1be650c0a49837d262149
Jose Cuevas <josecueram@gmail.com>
2020-07-31 19:04:58 -0500

    [ADD] Created BigCardOptions Component

---
+++ src/components/BigCardOptions/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export const BigCardOptions = ({ children }) => {
+  return (
+    <Container>
+      {
+        children
+      }
+    </Container>
+  )
+}
---
+++ src/components/BigCardOptions/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 10%;
+  background: var(--element);
+  border-radius: var(--border);
+  box-shadow: var(--shadow);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`
