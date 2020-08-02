0ddc0a7c247daeffb682a23dd0de340edbaa4e88
Jose Cuevas <josecueram@gmail.com>
2020-08-01 21:27:17 -0500

    [ADD] currentCard state reseting in Sets Page

--- src/hooks/useFetchSets.js
+++ src/hooks/useFetchSets.js
@@ -1,12 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import axios from 'axios'
+import { Context } from '../Context'
 import { apiUrl } from '../config'
 
 export const useFetchSets = () => {
+  const { setCurrentCard } = useContext(Context)
   const [sets, setSets] = useState({})
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setCurrentCard({})
     const fetchData = async () => {
       try {
         const { data: { sets } } = await axios.get(`${apiUrl}/sets`)
