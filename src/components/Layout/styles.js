153b8470bee1d05a5c80e045ef66733dce4a1ea1
Jose Cuevas <josecueram@gmail.com>
2020-08-04 00:37:07 -0500

    [UPDATE] Layout component spacing

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -2,14 +2,13 @@ import styled from 'styled-components'
 
 export const Arranger = styled.div`
   display: grid;
-  grid-template-columns: 70px 1fr;
+  grid-template-columns: 60px 1fr;
   grid-template-rows: 100vh;
-  gap: 15px;
 `
 
 export const Main = styled.main`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr;
-  padding: 10px;
+  padding: 10px 20px 0 0;
 `
