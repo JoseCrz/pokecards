94a08a347d7fe78a84e9ff089518f7d049ae9860
Jose Cuevas <josecueram@gmail.com>
2020-08-04 00:37:31 -0500

    [UPDATE] Favorites page elements positions

--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -4,13 +4,16 @@ export const Grid = styled.div`
   display: grid;
   grid-template-columns: 2fr 1fr;
   grid-template-rows: 150px 1fr;
-  grid-template-areas: 'banner banner'
+  grid-template-areas: 'cardList banner'
                        'cardList bigCard';
   overflow: scroll;
 `
 
 export const BannerContainer = styled.div`
   grid-area: banner;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export const CardListContainer = styled.div`
