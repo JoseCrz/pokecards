7b0378544856c4c822210fc6d9da4d57b50b2c9d
Jose Cuevas <josecueram@gmail.com>
2020-08-04 00:33:31 -0500

    [UPDATE] InputSearch proportions

--- src/components/InputSearch/styles.js
+++ src/components/InputSearch/styles.js
@@ -4,14 +4,16 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 export const Container = styled.div`
   display: flex;
+  width: 100%;
 `
 
 export const Input = styled.input`
-  padding: 5px;
+  padding: 8px 16px;
   border: none;
-  border-top-left-radius: var(--border);
-  border-bottom-left-radius: var(--border);
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
   color: gray;
+  width: 80%;
 
   &:focus {
     outline-color: var(--primary);
@@ -24,12 +26,12 @@ export const Button = styled.button`
   justify-content: center;
   align-items: center;
   border: none;
-  border-top-right-radius: var(--border);
-  border-bottom-right-radius: var(--border);
-  width: 35px;
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+  width: 20%;
 `
 
 export const Icon = styled(AiOutlineSearch)`
   color: var(--text-white);
-  font-size: 32px;
+  font-size: 28px;
 `
