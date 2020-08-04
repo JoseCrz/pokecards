4dce2fb6748be0a423755195083f9d559247ab1f
Jose Cuevas <josecueram@gmail.com>
2020-08-04 00:36:10 -0500

    [UPDATE] CardList  component alignment

--- src/components/CardList/styles.js
+++ src/components/CardList/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, 180px);
   grid-auto-rows: 251px;
-  gap: 20px;
+  gap: 30px;
   padding: 20px 25px;
-  /* justify-content: center; */
+  justify-content: center;
 `
