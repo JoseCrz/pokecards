f296ab0f37246621a3460cd017a3b7a03edfd8ef
Jose Cuevas <josecueram@gmail.com>
2020-08-04 20:39:02 -0500

    [FIX] duplicate fav bug

--- src/utils/favUtils.js
+++ src/utils/favUtils.js
@@ -1,8 +1,13 @@
 export const addFav = ({ card }) => {
   // TODO: Duplicate Handling
   const favs = JSON.parse(window.localStorage.getItem('favs')) || []
-  favs.push(card)
-  window.localStorage.setItem('favs', JSON.stringify(favs))
+
+  const ids = favs.map(fav => fav.id)
+
+  if (!ids.includes(card.id)) {
+    favs.push(card)
+    window.localStorage.setItem('favs', JSON.stringify(favs))
+  }
 }
 
 export const getFavs = () => JSON.parse(window.localStorage.getItem('favs')) || []
