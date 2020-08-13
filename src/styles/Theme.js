c6ae043fe7d9a75c3b3a351527b7f76c69d4d5c7
Jose Cuevas <josecueram@gmail.com>
2020-08-13 01:24:01 -0500

    [ADD] Created Basic Light and Dark Themes

---
+++ src/styles/Theme.js
@@ -0,0 +1,34 @@
+export const Theme = {
+  light: {
+    colors: {
+      background: '#F3F5FD',
+      primary: '#495DE3',
+      primaryLight: '#ECF9FF',
+      element: '#FFF',
+      text: '#252525',
+      textWhite: '#FFF',
+      textStrong: '#00087F',
+      icon: '#2B316C'
+    },
+    shadows: {
+      shadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+      shadowBig: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
+    }
+  },
+  dark: {
+    colors: {
+      background: '#232528',
+      primary: '#C7D732',
+      primaryLight: '#ECF9FF',
+      element: '#232528',
+      text: '#E9EAEA',
+      textWhite: '#FFF',
+      textStrong: '#F1F1F1',
+      icon: '#B2B3B4'
+    },
+    shadows: {
+      shadow: '-2px -2px 6px #383b40, 6px 6px 6px rgba(0,0,0,.24)',
+      shadowBig: '-2px -2px 6px #383b40, 6px 6px 6px rgba(0,0,0,.24)'
+    }
+  }
+}
