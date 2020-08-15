df328a5b3295311fe79a3880be6a471323cf2d6a
Jose Cuevas <josecueram@gmail.com>
2020-08-14 21:32:38 -0500

    [ADD] Implemented PaletteList in Settings Page

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 import { Layout } from '../../components/Layout'
-import { ThemeSwitcher } from '../../components/ThemeSwitcher'
+import { PaletteList } from '../../components/PaletteList'
+
+import { H1, ListGrid } from './styles'
 
 export const Settings = () => {
   return (
     <Layout title='Settings'>
-      <ThemeSwitcher />
+      <div>
+        <H1>Select a theme</H1>
+        <ListGrid>
+          <PaletteList />
+        </ListGrid>
+      </div>
     </Layout>
   )
 }
---
+++ src/pages/Settings/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+  text-align: center;
+`
+
+export const ListGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  grid-auto-rows: 150px;
+  gap: 15px;
+  padding: 15px;
+`
