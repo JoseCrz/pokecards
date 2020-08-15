03adcd7342642fd09160e2761b89e3da0ec611c2
Jose Cuevas <josecueram@gmail.com>
2020-08-14 22:00:09 -0500

    [UPDATE] Implemented accent var in LinkStyled

--- src/components/LinkStyled/styles.js
+++ src/components/LinkStyled/styles.js
@@ -4,15 +4,15 @@ import { Link } from '@reach/router'
 export const StyledLink = styled(Link)`
   background: transparent;
   border-radius: var(--border);
-  border: 1px solid var(--primary);
+  border: 1px solid var(--accent);
   padding: 5px 10px;
-  color: var(--primary);
+  color: var(--accent);
   transition: cubic-bezier(0.215, 0.610, 0.355, 1) .2s;
   text-decoration: none;
   margin: 0px 10px;
   
   &:hover {
-    background: var(--primary);
+    background: var(--accent);
     color: var(--text-white);
   }
   
