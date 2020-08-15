8ee644bf260c1f2935e6a261c9e3f2b7193d99cb
Jose Cuevas <josecueram@gmail.com>
2020-08-14 21:31:47 -0500

    [ADD] New theme for testing

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,5 +1,6 @@
 export const Theme = {
   light: {
+    name: 'Light',
     colors: {
       background: '#F3F5FD',
       primary: '#495DE3',
@@ -16,6 +17,7 @@ export const Theme = {
     }
   },
   dark: {
+    name: 'Dark',
     colors: {
       background: '#232528',
       primary: '#C7D732',
@@ -29,6 +31,24 @@ export const Theme = {
     shadows: {
       shadow: '-2px -2px 6px #383b40, 6px 6px 6px rgba(0,0,0,.24)',
       shadowBig: '-2px -2px 6px #383b40, 6px 6px 6px rgba(0,0,0,.24)'
+    }
+  },
+  fireRed: {
+    name: 'Fire Red',
+    colors: {
+      background: '#FFFFFA',
+      primary: '#890620',
+      primaryLight: '#ECF9FF',
+      element: '#FFF',
+      text: '#353535',
+      textWhite: '#FFF',
+      textStrong: '#353535',
+      icon: '#FF5964',
+      accent: '#FFAE03'
+    },
+    shadows: {
+      shadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+      shadowBig: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
     }
   }
 }
