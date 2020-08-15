996080d07e58c8efd187c65c4d1494c3c91ba622
Jose Cuevas <josecueram@gmail.com>
2020-08-15 16:21:40 -0500

    [UPDATE]  Sets Page mobile styles

--- src/pages/Sets/styles.js
+++ src/pages/Sets/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
+import { phone } from '../../styles/MediaQueries'
 
 export const SetListContainer = styled.div`
   max-height: 100%;
   overflow: scroll;
+
+  ${phone} {
+    width: 100vw;
+    padding: 0 15px;
+  }
 `
