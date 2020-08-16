fbbd3d495a9bdf5b8c811a58bc84ad79b44ffccd
Jose Cuevas <josecueram@gmail.com>
2020-08-15 23:53:31 -0500

    [ADD] Closing Modal functionality

--- src/components/BigCardModal/index.jsx
+++ src/components/BigCardModal/index.jsx
@@ -1,22 +1,20 @@
 import React, { useContext } from 'react'
-import { Context } from '../../Context'
 import ReactDOM from 'react-dom'
+import { Context } from '../../Context'
 
 import { BigCard } from '../BigCard'
 
 import { Container, Content } from './styles'
 
 export const BigCardModal = ({ isOpen }) => {
-  console.log('BigCardModal -> isOpen', isOpen)
+  const { setIsModalOpen } = useContext(Context)
+  if (!isOpen) return null
 
-  if (isOpen) {
-    return ReactDOM.createPortal(
-      <Container>
-        <Content>
-          <BigCard />
-        </Content>
-      </Container>
-      , document.querySelector('#modal'))
-  }
-  return null
+  return ReactDOM.createPortal(
+    <Container onClick={() => setIsModalOpen(false)}>
+      <Content>
+        <BigCard />
+      </Content>
+    </Container>
+    , document.querySelector('#modal'))
 }
