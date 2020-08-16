352aeb6042142818fbc28d88c0883ff805d8e999
Jose Cuevas <josecueram@gmail.com>
2020-08-16 03:23:53 -0500

    [UPDATE] SetBanner styles for  mobile

--- src/components/SetBanner/styles.js
+++ src/components/SetBanner/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { phone } from '../../styles/MediaQueries'
 
 export const Banner = styled.div`
   height: 100%;
@@ -10,13 +11,29 @@ export const Banner = styled.div`
 
 export const TextContainer = styled.div`
   display: flex;
+  
+  ${phone} {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 5px;
+  }
 `
 
 export const P = styled.p`
   margin-right: 10px;
   color: darkgray;
+  
+  ${phone} {
+    text-align: center;
+  }
 `
 
 export const Img = styled.img`
   max-height: 60%;
+  
+  ${phone} {
+    padding-top: 20px;
+    max-height: 80px;
+    max-width: 60%;
+  }
 `
