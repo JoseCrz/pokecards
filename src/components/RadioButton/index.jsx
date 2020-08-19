3ab447b23ef59600605f676011ce53e4cc8c5c67
Jose Cuevas <josecueram@gmail.com>
2020-08-19 00:24:22 -0500

    [DELETE] Unused Component

--- src/components/RadioButton/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Button } from './styles'
-
-export const RadioButton = ({ onClick, children }) => {
-  return (
-    <Button onClick={onClick}>{children}</Button>
-  )
-}
