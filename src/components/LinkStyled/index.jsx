1b8f75fa632b4b0fa05e5b8b540c2818ef398bd7
Jose Cuevas <josecueram@gmail.com>
2020-08-19 00:09:48 -0500

    [ADD] PropTypes -> LinkStyled

--- src/components/LinkStyled/index.jsx
+++ src/components/LinkStyled/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 import { StyledLink } from './styles'
 
 export const LinkStyled = ({ to, onClick, children }) => {
   return onClick
     ? <StyledLink to={to} onClick={onClick}>{children}</StyledLink>
     : <StyledLink to={to}>{children}</StyledLink>
+}
+
+LinkStyled.propTypes = {
+  to: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 }
