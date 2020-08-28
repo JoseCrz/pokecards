420b0995f06b013111af14ce4dfde828d9a74803
Jose Cuevas <josecueram@gmail.com>
2020-08-27 21:53:35 -0500

    [UPDATE] Improved performance of Card & CardList

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { Context } from '../../Context'
 
@@ -8,42 +8,44 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { Container, Img, Figure } from './styles'
 
-export const Card = ({ card }) => {
-  const [isLoading, setIsLoading] = useState(true)
-  const { currentCard, setCurrentCard, setBigPictureLoaded, toggleFav, activateModal } = useContext(Context)
+export const Card = React.memo(
+  ({ card }) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const { currentCard, setCurrentCard, setBigPictureLoaded, toggleFav, activateModal } = useContext(Context)
 
-  const [show, element] = useNearScreen()
+    const [show, element] = useNearScreen()
 
-  const handleOnClick = () => {
-    if (currentCard === null) {
-      setBigPictureLoaded(false)
-      setCurrentCard(card)
-    } else {
-      if (currentCard.id !== card.id) {
+    const handleOnClick = () => {
+      if (currentCard === null) {
         setBigPictureLoaded(false)
         setCurrentCard(card)
+      } else {
+        if (currentCard.id !== card.id) {
+          setBigPictureLoaded(false)
+          setCurrentCard(card)
+        }
       }
+
+      activateModal()
     }
 
-    activateModal()
+    const handleFav = useCallback(() => toggleFav({ card }), [toggleFav])
+
+    return (
+      <Container ref={element}>
+        {
+          show &&
+            <>
+              <FavButton onClick={handleFav} cardId={card.id} />
+              <Figure onClick={handleOnClick}>
+                <Img isloading={isLoading} src={card.imageUrl} onLoad={() => setIsLoading(false)} alt='' />
+              </Figure>
+            </>
+        }
+      </Container>
+    )
   }
-
-  const handleFav = () => toggleFav({ card })
-
-  return (
-    <Container ref={element}>
-      {
-        show &&
-          <>
-            <FavButton onClick={handleFav} cardId={card.id} />
-            <Figure onClick={handleOnClick}>
-              <Img isloading={isLoading} src={card.imageUrl} onLoad={() => setIsLoading(false)} alt='' />
-            </Figure>
-          </>
-      }
-    </Container>
-  )
-}
+)
 
 Card.propTypes = {
   card: PropTypes.object.isRequired
--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -4,18 +4,19 @@ import PropTypes from 'prop-types'
 import { Card } from '../Card'
 
 import { Container, H3 } from './styles'
-export const CardList = ({ cards = [] }) => {
-  return (
-    <Container>
-      {
-        cards.length > 0
-          ? cards.map(card => <Card key={card.id} card={card} />)
-          : <H3>We couldn't find that...</H3>
-      }
-    </Container>
-  )
-}
-
+export const CardList = React.memo(
+  ({ cards = [] }) => {
+    return (
+      <Container>
+        {
+          cards.length > 0
+            ? cards.map(card => <Card key={card.id} card={card} />)
+            : <H3>We couldn't find that...</H3>
+        }
+      </Container>
+    )
+  }
+)
 CardList.propTypes = {
   cards: PropTypes.array.isRequired
 }
